66f2e0d77b219367d585ab62951df5ec62be7d44
mjsmith <mjsmith@sonic.net>
2015-12-05 20:05:46 -0800

    Main

--- FinalProject/FinalProject/main.cpp
+++ FinalProject/FinalProject/main.cpp
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #include "GraphSearch.h"
+#include "ParallelSearch.h"
 #include "TravellingSalesman.h"
 #include "cities.h"
 
@@ -16,48 +17,40 @@ int main(int argc, const char * argv[]) {
     std::vector<unsigned int> previous;
     previous.push_back(0);
     TravellingSalesman start(0,0,0,previous);
-    
+    /*
     std::cout << "======UCS Result======" << std::endl;
     auto result = graphSearch.UniformSearch(start, start);
     std::cout << "Explored Size: " << graphSearch.getLastExploredCount() << std::endl;
     for (size_t i=0; i<result.size(); i++) {
         result.at(i).printNode();
     }
-    
+    */
     std::cout << std::endl << "======UCS BnB Result======" << std::endl;
-    result = graphSearch.UniformSearchBnB(start, start);
+    auto result = graphSearch.UniformSearchBnB(start, start);
     std::cout << "Explored Size: " << graphSearch.getLastExploredCount() << std::endl;
     for (size_t i=0; i<result.size(); i++) {
         result.at(i).printNode();
     }
-    
+    /*
     std::cout << std::endl << "======A* Result======" << std::endl;
     result = graphSearch.AStarSearch(start, start);
     std::cout << "Explored Size: " << graphSearch.getLastExploredCount() << std::endl;
     for (size_t i=0; i<result.size(); i++) {
         result.at(i).printNode();
     }
-    
-    // too slow for more than 5 or 6 cities
-    /*
-     std::cout << std::endl << "======BFS Result======" << std::endl;
-     result = graphSearch.BreadthFirstSearch(start, start);
-     std::cout << "Explored Size: " << graphSearch.getLastExploredCount() << std::endl;
-     for (size_t i=0; i<result.size(); i++) {
-     result.at(i).printNode();
-     }
-     
-     std::cout << std::endl << "======DFS Result======" << std::endl;
-     result = graphSearch.DepthFirstSearch(start, start);
-     std::cout << "Explored Size: " << graphSearch.getLastExploredCount() << std::endl;
-     for (size_t i=0; i<result.size(); i++) {
-     result.at(i).printNode();
-     }
-     */
-    
-    std::cout << std::endl << "======Parallel UCS B&B Result======" << std::endl;
+    */
+    std::cout << std::endl << "======Max's Parallel UCS B&B Result======" << std::endl;
     result = graphSearch.ParallelUniformCostSearchBnB(start, start);
     std::cout << "Explored Size: " << graphSearch.getLastExploredCountAtomic() << std::endl;
+    for (size_t i=0; i<result.size(); i++) {
+        result.at(i).printNode();
+    }
+    
+    // My impl
+    ParallelSearch<TravellingSalesman> parallelSearch;
+    std::cout << std::endl << "======Matt's Parallel UCS B&B Result======" << std::endl;
+    result = parallelSearch.ParallelGraphSearch(start, start, 4);
+    //std::cout << "Explored Size: " << graphSearch.getLastExploredCountAtomic() << std::endl;
     for (size_t i=0; i<result.size(); i++) {
         result.at(i).printNode();
     }
