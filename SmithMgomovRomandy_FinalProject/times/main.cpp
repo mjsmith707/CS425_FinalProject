0c3f57e6b61d770521c7ae53ae8b0f341c86e011
mjsmith <mjsmith@sonic.net>
2015-12-08 21:42:15 -0800

    Cleanup /times

--- SmithMgomovRomandy_FinalProject/times/main.cpp
+++ SmithMgomovRomandy_FinalProject/times/main.cpp
@@ -1,13 +1,34 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
+#include <algorithm>
+#include <string>
 
-int main() {
+int main(int argc, char** argv) {
+	if (argc < 4) {
+		std::cout << "Usage: ./a.out ID numIterations inputTimes.txt" << std::endl;
+		std::cout << "numEntries is how many overall iteration were run for averaging purposes" << std::endl;
+		std::cout << "Example Tags: 0 for parallel, 1 for Rivoire's DFS, 2 for serialDfs" << std::endl;
+		exit(-1);
+	}
+
 	std::fstream input;
-	input.open("dfsserialtimes.txt");
-	double times[33][23];
+	input.open(argv[3]);
+	if (!input.is_open()) {
+		std::cout << "Failed to open file!" << std::endl;
+		exit(-2);
+	}
+
+	int tag = std::stoi(argv[1]);
+	int iterations = std::stoi(argv[2]);
+	if (iterations == 0) {
+		std::cout << "Don't divide by zero!" << std::endl;
+		exit(-3);
+	}
+
+	double times[33][40];
 	for (int i=0; i<33; i++) {
-		for (int j=0; j<23; j++) {
+		for (int j=0; j<40; j++) {
 			times[i][j] = 0.0f;
 		}
 	}
@@ -17,19 +38,22 @@ int main() {
 	int numcities;
 	double time;
 	char comma;
+
 	while (input >> algo >> comma >> numthreads >> comma >> numcities >> comma >> time) {
 		times[numthreads][numcities] += time;
 	}
 
 	for (int i=0; i<33; i++) {
-		for (int j=0; j<23; j++) {
-			times[i][j] /= 3;
+		for (int j=0; j<40; j++) {
+			times[i][j] /= iterations;
 		}
 	}
 
 	for (int i=1; i<33; i*=2) {
-		for (int j=5; j<23; j++) {
-			std::cout << std::fixed << std::setprecision(9) << "2," << i << "," << j << "," << times[i][j] << std::endl;
+		for (int j=0; j<40; j++) {
+			if (times[i][j] > 0.0f) {
+				std::cout << std::fixed << std::setprecision(9) << tag << "," << i << "," << j << "," << times[i][j] << std::endl;
+			}
 		}
 	}
 }
