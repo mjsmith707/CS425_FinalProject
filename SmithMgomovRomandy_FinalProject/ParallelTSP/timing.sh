20a84f879c7d699dd0290ce37b7c57971f5f799c
mjsmith <mjsmith@sonic.net>
2015-12-08 21:00:43 -0800

    Cleanup Timing Scripts

--- SmithMgomovRomandy_FinalProject/ParallelTSP/timing.sh
+++ SmithMgomovRomandy_FinalProject/ParallelTSP/timing.sh
@@ -1,31 +1,56 @@
 #!/bin/bash
-counter=0
-city=5
-cityUpperbound=31
-citiesbase="cities/cities"
-citiesext=".h"
-citiesfile=""
-numthreads=1
+# Automated timing script
+# Set #undef VERBOSE in main.cpp to generate csv output
+# The number of times to run the for number of threads and cities
+iterations=3
+
+# The first city to start with. This corresponds to cities/city#.h
+cityMin=5
+
+# The upper bound on how many cities to attempt
+cityUpperBound=15
+
+# Location of cities headers
+citiesBase="cities/cities"
+citiesExt=".h"
+
+# The amount of threads to start with
+# incremented by *2
+numThreadsMin=1
+
+# The amount of threads to stop at
+numThreadsMax=32
+
+# How long to run the program before giving up
+timeoutDuration="10m"
+
+# Output file
 output="times.txt"
+
+# Script Start
 retcode=0
-
-
+counter=0
+citiesFile=""
 make clean
 rm ./$output
 
-while [ $counter -lt 4 ]; do
-	let numthreads=1
-	while [ $numthreads -lt 2 ]; do
-		let city=5;
-		while [ $city -lt $cityUpperbound ]; do
-			citiesfile=$citiesbase$city$citiesext
-			cp ./$citiesfile ./cities.h
+while [ $counter -lt $iterations ]; do
+	let numthreads=$numThreadsMin
+	while [ $numthreads -le $numThreadsMax ]; do
+		let city=$cityMin;
+		while [ $city -le $cityUpperBound ]; do
+			# Copy in new cities.h
+			citiesFile=$citiesBase$city$citiesExt
+			cp ./$citiesFile ./cities.h
+			# Rebuild
 			make clean && make
-			timeout 10m ./project.x $numthreads >> $output
+			timeout $timeoutDuration ./project.x $numthreads >> $output
+			# Check return code
+			# if failed then skip the rest of the cities
 			retcode=$?
 			if [[ $retcode != 0 ]];
 			then
-				let city=$cityUpperbound
+				let city=$cityUpperBound+1
 			else
 				let city=city+1
 			fi
--- SmithMgomovRomandy_FinalProject/SerialDFS/timing.sh
+++ SmithMgomovRomandy_FinalProject/SerialDFS/timing.sh
@@ -1,35 +1,50 @@
 #!/bin/bash
-counter=0
-city=5
-citiesbase="cities/cities"
-citiesext=".h"
-citiesfile=""
-numthreads=1
+# Automated timing script
+# Set #undef VERBOSE in main.cpp to generate csv output
+# The number of times to run the for number cities
+iterations=3
+
+# The first city to start with. This corresponds to cities/city#.h
+cityMin=5
+
+# The upper bound on how many cities to attempt
+cityUpperBound=15
+
+# Location of cities headers
+citiesBase="cities/cities"
+citiesExt=".h"
+
+# How long to run the program before giving up
+timeoutDuration="10m"
+
+# Output file
 output="times.txt"
+
+# Script Start
 retcode=0
-
-
+counter=0
+citiesFile=""
 make clean
 rm ./$output
 
-#while [ $counter -lt 4 ]; do
-	let numthreads=1
-	while [ $numthreads -lt 2 ]; do
-		let city=5;
-		while [ $city -lt 21 ]; do
-			citiesfile=$citiesbase$city$citiesext
-			cp ./$citiesfile ./cities.h
-			make clean && make
-			timeout 10m ./serialdfs.x $numthreads >> $output
-			retcode=$?
-			if [[ $retcode != 0 ]];
-			then
-				let city=21
-			else
-				let city=city+1
-			fi
-		done
-		let numthreads=numthreads*2
+while [ $counter -lt $iterations ]; do
+	let city=$cityMin;
+	while [ $city -le $cityUpperBound ]; do
+		# Copy in new cities.h
+		citiesFile=$citiesBase$city$citiesExt
+		cp ./$citiesFile ./cities.h
+		# Rebuild
+		make clean && make
+		timeout $timeoutDuration ./serialdfs.x >> $output
+		# Check return code
+		# if failed then skip the rest of the cities
+		retcode=$?
+		if [[ $retcode != 0 ]];
+		then
+			let city=$cityUpperBound+1
+		else
+			let city=city+1
+		fi
 	done
 	let counter=counter+1
-#done
+done
